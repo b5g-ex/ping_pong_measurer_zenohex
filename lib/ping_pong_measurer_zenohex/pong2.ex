4b54d11428b2bd8b8ae045bfda0f72c491526945
s-hosoai <shintaro.hosoai@gmail.com>
2023-07-06 12:00:33 +0900

    change heredoc to comment

--- lib/ping_pong_measurer_zenohex/pong2.ex
+++ lib/ping_pong_measurer_zenohex/pong2.ex
@@ -13,30 +13,29 @@ defmodule PingPongMeasurerZenohex.Pong2 do
     GenServer.start_link(__MODULE__, args_tuple, name: __MODULE__)
   end
 
-  def init({context, node_counts}) when is_integer(node_counts) do
-    """
-    {:ok, node_id_list} = Rclex.ResourceServer.create_nodes(context, 'pong_node', node_counts)
+  def init({session, node_counts}) when is_integer(node_counts) do
+    # {:ok, node_id_list} = Rclex.ResourceServer.create_nodes(context, 'pong_node', node_counts)
 
-    {:ok, subscribers} =
-      Rclex.Node.create_subscribers(node_id_list, @message_type, @ping_topic, :multi)
+    # {:ok, subscribers} =
+    #   Rclex.Node.create_subscribers(node_id_list, @message_type, @ping_topic, :multi)
 
-    {:ok, publishers} =
-      Rclex.Node.create_publishers(node_id_list, @message_type, @pong_topic, :multi)
+    # {:ok, publishers} =
+    #   Rclex.Node.create_publishers(node_id_list, @message_type, @pong_topic, :multi)
 
-    for {_node_id, index} <- Enum.with_index(node_id_list) do
-      subscriber = Enum.at(subscribers, index)
-      publisher = Enum.at(publishers, index)
+    # for {_node_id, index} <- Enum.with_index(node_id_list) do
+    #   subscriber = Enum.at(subscribers, index)
+    #   publisher = Enum.at(publishers, index)
 
-      Rclex.Subscriber.start_subscribing([subscriber], context, fn message ->
-        message = Rclex.Msg.read(message, @message_type)
-        Logger.debug('ping: ' ++ message.data)
+    #   Rclex.Subscriber.start_subscribing([subscriber], context, fn message ->
+    #     message = Rclex.Msg.read(message, @message_type)
+    #     Logger.debug('ping: ' ++ message.data)
 
-        Rclex.Publisher.publish([publisher], [Utils.create_payload(message.data)])
-      end)
-    end
+    #     Rclex.Publisher.publish([publisher], [Utils.create_payload(message.data)])
+    #   end)
+    # end
 
-    {:ok, nil}
-  """
+    # {:ok, nil}
+
     session = Zenohex.open
     {:ok, publisher} = Session.declare_publisher(session, "demo/example/zenoh-rs-pub")
     {:ok, subscriber} = Session.declare_subscriber(session, "demo/example/zenoh-rs-pub", fn m -> IO.inspect(m) end)
